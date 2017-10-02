f20b4f4bb5c79410dea219a2b910f061c6df5fb2
Andreas Marfurt <andreas.marfurt@gmail.com>
2017-10-02 17:03:04 +0200

    Correctly stopping to consume messages.

--- workers/rmq_worker.py
+++ workers/rmq_worker.py
@@ -8,6 +8,8 @@ import logging
 import threading
 from abc import ABC, abstractmethod
 
+TIMEOUT = 1  # in seconds
+
 
 class Rabbit(threading.Thread, ABC):
 
@@ -19,39 +21,44 @@ class Rabbit(threading.Thread, ABC):
         self.host = host
         self.queue = queue
         self.log = logging.getLogger(self.name)
+        self.stopped = False
 
     def run(self):
-        while True:
+        while not self.stopped:
             try:
                 self.connection = pika.BlockingConnection(pika.ConnectionParameters(host=self.host))
                 self.channel = self.connection.channel()
                 self.channel.queue_declare(queue=self.queue, durable=True)
                 self.channel.basic_qos(prefetch_count=1)
                 self.channel.basic_consume(self.callback, queue=self.queue)
+                self.log.debug('Starting to consume...')
+                self.check_alive()
                 self.channel.start_consuming()
+                self.log.debug('Stopped consuming')
             except pika.exceptions.ConnectionClosed:
                 # reconnect after timeout
                 self.log.debug('Connection closed. Reconnecting...')
-                pass
             except:
                 # something is wrong, exiting
                 self.log.exception('Unexpected error')
                 self.stop()
-                break
+
+    def check_alive(self):
+        """ Checks whether this worker is still active, otherwise shuts it down. """
+        if not self.stopped:
+            self.connection.add_timeout(TIMEOUT, self.check_alive)
+        else:
+            try:
+                self.connection.close()
+                self.log.info('Closed connection')
+            except pika.exceptions.ConnectionClosed:
+                self.log.debug('Connection already closed when stopping the worker.')
 
     def stop(self):
         """ Tells this worker to stop consuming messages. """
-        try:
-            self.log.info('Stopping worker...')
-            # TODO: is this blocking?
-            self.channel.stop_consuming()
-            self.log.info('Stopped consuming')
-            self.connection.close()
-            self.log.info('Closed connection')
-        except pika.exceptions.ConnectionClosed:
-            # connection already closed, exiting
-            self.log.debug('Connection already closed when stopping the worker.')
-            pass
+        self.stopped = True
+        self.log.info('Stopping worker...')
+        self.channel.stop_consuming()
 
     @abstractmethod
     def callback(self, channel, method, properties, body):
